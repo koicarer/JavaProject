3bd65042ca6ee39e2566617fe3f4d1f6c8a6d04d
Chris Choi <choicd@hotmail.com>
2018-12-15 17:01:05 -0500

    readme

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,6 @@
 public class HelloJenkins {
      public static void main (String[] args) {
             System.out.println ("Hello Jenkins!");
+            System.out.println ("Chris loves Chloe!");
      }
 }
